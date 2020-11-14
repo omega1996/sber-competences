b52dc3b676eeb90badedc3a92f6b779efe30c3a4
IvanR <yu181219710>
2020-11-14 18:57:49 +0500

    removed trash

--- app/app.py
+++ app/app.py
@@ -1,18 +1,11 @@
-from flask import Flask, render_template, Markup
-from flask_cors import CORS
+from flask import Flask, render_template
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    all_spec = Markup('a')
     return render_template('index.html')
-
-@app.route('/firsttry')
-def firsttry():
-    return render_template('second.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
