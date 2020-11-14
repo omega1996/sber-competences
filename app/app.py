1ede65a309dfe141dcc6021cd0a22b01255caace
IvanR <yu181219710>
2020-11-14 22:47:19 +0500

    added dynamic routes

--- app/app.py
+++ app/app.py
@@ -9,9 +9,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/frontend')
-def frontend():
-    return render_template('frontend/index.html')
+
+@app.route('/profession/<prof_name>')
+def frontend(prof_name):
+    return render_template(prof_name+'/index.html')
 
 
 if __name__ == '__main__':
