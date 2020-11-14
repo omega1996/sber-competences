d92aa5bc258b1cfcbd176efee47eb2a4469d7990
Vladimir Fomenko <vlakoresker@gmail.com>
2020-11-15 00:06:34 +0500

    add get_spec_stat

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 import pandas as pd
+import json
 
 app = Flask(__name__)
 
-stat_tech_data = pd.read_csv('stat_tech_data.csv')
+stat_tech_data = pd.read_csv('data/stat_tech_data.csv')
+spec_stat = pd.read_csv('data/spec_stat.csv')
+
 
 @app.route('/')
 @app.route('/index')
@@ -24,6 +27,15 @@ def get_top_tech(specialization, year, month):
         & (stat_tech_data.year == int(year))
         & (stat_tech_data.month == int(month))
     ].tags.iloc[0]
+
+
+@app.route('/get_spec_stat/<year>/<month>')
+def get_spec_stat(year, month):
+    month_stat = spec_stat[
+        (spec_stat.year == int(year))
+        & (spec_stat.month == int(month))
+    ]
+    return json.dumps(month_stat[['spec', 'freq']].to_dict('record'))
 
 
 if __name__ == '__main__':
