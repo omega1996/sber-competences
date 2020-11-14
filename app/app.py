c6d2ac3f276bb67c09599c46433421744d19a0f3
Vladimir Fomenko <vlakoresker@gmail.com>
2020-11-14 23:28:27 +0500

    add get_top_tech

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
+stat_tech_data = pd.read_csv('stat_tech_data.csv')
 
 @app.route('/')
 @app.route('/index')
@@ -9,9 +11,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/frontend')
 def frontend():
     return render_template('frontend/index.html')
+
+
+@app.route('/get_top_tech/<specialization>/<year>/<month>')
+def get_top_tech(specialization, year, month):
+    return stat_tech_data[
+        (stat_tech_data.spec == specialization)
+        & (stat_tech_data.year == int(year))
+        & (stat_tech_data.month == int(month))
+    ].tags.iloc[0]
 
 
 if __name__ == '__main__':
