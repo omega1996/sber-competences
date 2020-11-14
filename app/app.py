0dbdec677c2e5eeecb2fe96049f40d5e03171218
IvanR <yu181219710>
2020-11-14 20:30:07 +0500

    added folders and frontend page

--- app/app.py
+++ app/app.py
@@ -5,8 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
+
+@app.route('/frontend')
+def frontend():
+    return render_template('frontend/index.html')
 
 
 if __name__ == '__main__':
